45f8eff09ea811025a039af9733114f596f08490
QUANG221222 <nguyennhatquang.2509@gmail.com>
2025-08-01 11:45:41 +0700

    Add task routes and HTTP status codes support; remove unused example route

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -2,9 +2,16 @@
 import express from 'express'
 import { env } from '~/config/environment'
 import { CONNECT_DB } from '~/config/mongodb'
+import { APIs_V1 } from '~/routes/v1/index.js'
 
 const START_SERVER = () => {
   const app = express()
+
+  app.get('/', (req, res) => {
+    res.send(`Welcome to the ToDo List App! Author: ${env.AUTHOR_NAME}`)
+  })
+
+  app.use('/v1', APIs_V1)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`Server is running at http://${env.APP_HOST}:${env.APP_PORT}`)
