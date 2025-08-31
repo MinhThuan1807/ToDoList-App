2ec52d7bd9ba17f09157fc1c47802830a77b4899
QUANG221222 <nguyennhatquang.2509@gmail.com>
2025-08-31 21:57:49 +0700

    feat: enhance UI and improve user experience; update styles, add tooltips, and modify API error handling

--- FrontEnd/src/components/Action.tsx
+++ FrontEnd/src/components/Action.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 function Action() {
   return (
-    <div className="absolute right-0 w-[200px] flex justify-center items-center gap-5 ">
+    <div className="absolute right-1 top-3 sm:right-0 sm:w-[200px] flex justify-center items-center gap-5 w-[100px]">
       <Link
         to="/register"
         className="w-[75px] p-2 bg-brand-light rounded-2xl cursor-pointer hover:bg-brand-dark transition delay-100 duration-75 ease-in-out animate-ping scale-75"
