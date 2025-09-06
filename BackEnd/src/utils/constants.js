436c60433ad636ca92e317eacd03b7e0d77561c2
QUANG221222 <nguyennhatquang.2509@gmail.com>
2025-09-06 09:52:12 +0700

    feat: update whitelist domains to allow localhost access for API

--- BackEnd/src/utils/constants.js
+++ BackEnd/src/utils/constants.js
@@ -1,6 +1,6 @@
 import { env } from '~/config/environment'
 // Domains that are allowed to access the API
-export const WHITELIST_DOMAINS = ['']
+export const WHITELIST_DOMAINS = ['http://localhost:5173']
 
 export const WEBSITE_DOMAIN =
   env.BUILD_MODE === 'production'
