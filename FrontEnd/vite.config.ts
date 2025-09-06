19be288f0bb8dea6e1a4d3789efec65fa2833a97
QUANG221222 <nguyennhatquang.2509@gmail.com>
2025-09-06 09:49:14 +0700

    feat: update API configuration for environment variables; adjust whitelist domains and add cross-env for script consistency

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -5,8 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react({}), tailwindcss()],
-  // base: './'
-  resolve: {
-    alias: [{ find: '~', replacement: '/src' }]
+  define: {
+    'process.env': process.env
   }
 })
